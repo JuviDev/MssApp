98337756db57183bc6002a661dcb6a226bacbdf8
julcodes <julianve351@gmail.com>
2021-11-06 13:27:59 -0500

    gh-pages added

--- android/app/src/main/java/com/teus351/reactnativefirebase/MainApplication.java
+++ android/app/src/main/java/com/teus351/reactnativefirebase/MainApplication.java
@@ -1,4 +1,4 @@
-package com.reactnativefirebase;
+package com.teus351.reactnativefirebase;
 
 import android.app.Application;
 import android.content.Context;
@@ -11,7 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.reactnativefirebase.generated.BasePackageList;
+import com.teus351.reactnativefirebase.generated.BasePackageList;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
@@ -107,7 +107,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.reactnativefirebase.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.teus351.reactnativefirebase.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
